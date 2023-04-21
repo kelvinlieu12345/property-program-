fa74223de1ff0cf6bf43812b122197bb09e514c8
kelvinlieu12345 <51345212+kelvinlieu12345@users.noreply.github.com>
2023-04-22 01:09:44 +0800

    Create Property.py

---
+++ Property.py
@@ -0,0 +1,41 @@
+rooms = 'living room'
+kitchen='steam rice'
+bathroom='toilet and bathroom'
+agent = 'agent comission fee and other fee required normally take 4months if rental situation'
+childtakecare='school internet'
+transportation='station nearby'
+
+details = []
+
+details.append(rooms)
+details.append(kitchen)
+details.append(bathroom)
+details.append(agent)
+details.append(childtakecare)
+details.append(transportation)
+for detail in details:
+    print(detail)
+    
+#commercial store rental
+tax = 'religion tax policy apply'
+license='br fee required if business running'
+storerental='depends on agent or store owner'
+waterandeletricity='less than 200 hkd per month in this case example'
+
+shopdetail.append(tax)
+shopdetail.append(license)
+shopdetail.append(storerental)
+shopdetail.append(waterandelectrucity)
+for shopdetail in shopdetails:
+    print(shopdetail)
+    
+artrepresentation='space and education'
+educationoptions=['primary school','secondary school','college','online course e.g. codecademy']
+space=['time space except time travel','computer science','postcard membership','logistics and transportation','supplychain and express']
+
+list = []
+list.append(space)
+list.append(artrepresentation)
+list.append(educationoptions)
+for option in list:
+    print(option)
